91ab795a92b5b0553a9d179caafb5f7cb42b730d
apemother <joelmund@gmail.com>
2021-11-01 20:34:59 +0100

    Remove unnecessary renaming of simple

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,20 +1,20 @@
 """
 Holds tests
 """
-import src.simple as simp
+from src import simple
 
 
 def test_answer():
-    assert simp.increment(3) == 4
+    assert simple.increment(3) == 4
 
 
 def test_add():
-    assert simp.add(1, 3) == 4
+    assert simple.add(1, 3) == 4
 
 
 def test_subtract():
-    assert simp.subtract(15, 2) == 13
+    assert simple.subtract(15, 2) == 13
 
 
 def test_multiply():
-    assert simp.multiply(10, 10) == 100
+    assert simple.multiply(10, 10) == 100
